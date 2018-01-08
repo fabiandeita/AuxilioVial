9a619cbaa37be8df441af5b3db12ad6fcde32b1c
irisviridiana <iriscordobajob@gmail.com>
2018-01-07 18:26:14 -0600

    InicioNavegacion
    Se inciai a diseñar la navegación

--- AuxilioVial/ViewController/LoginVC.swift
+++ AuxilioVial/ViewController/LoginVC.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginVC: UIViewController {
+    @IBOutlet weak var ingresarButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnIngresar(_ sender: UIButton) {
+    }
+    
 }
 
